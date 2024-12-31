cee690d75912910a6b515210217945aa50a1890a
Heet Patel <heetpatel532004@gmail.com>
2024-12-31 20:57:35 +0530

    Updating BaseURL for smooth functioning

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast"; // For displaying toast notifications
 import { io } from "socket.io-client"; 
 
 // Base URL for the API
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; 
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; 
 
 export const useAuthStore = create((set, get) => ({
 
